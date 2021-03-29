addbecdf73699e92d07f4d6b3d341d6042cd8e29
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-03-29 12:26:31 -0500

    populated dropdown

--- scripts/snacks/SnackDetails.js
+++ scripts/snacks/SnackDetails.js
@@ -15,7 +15,9 @@ export const SnackDetails = (snackObject) => {
 						<div class="col col-details">Season: ${snackObject.season.name}</div>
 					</div>
 					<div class="row row-cols-1">
-						<div class="col col-details">${snackObject.toppings}</div>
+						<p class="col col-details">Toppings: ${snackObject.toppings.map(topping => 
+							topping.topping.name).join (", ")}</p>
+							 
 					</div>
 				</div>
 			  	
